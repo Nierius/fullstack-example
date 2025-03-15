989288e90aa0022360d9a0bb5ab939486b21c87e
Marius Niemenmaa <marius.niemenmaa@outlook.com>
2025-03-15 12:12:54 +0200

    Prepare for post interactions

--- fullstack-ui/src/app/user.service.ts
+++ fullstack-ui/src/app/user.service.ts
@@ -1,17 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { User } from '@app/types';
-import { Observable } from 'rxjs';
+import { NewUser, User } from '@app/types';
+import { BehaviorSubject, interval, Observable, shareReplay, Subject, switchMap, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private readonly _apiBaseUrl = "https://jsonplaceholder.typicode.com"
+  private readonly _userRefresh$ = new BehaviorSubject<void>(undefined);
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {
+    // Occasional fetch in case some some other user has changed user listing, could be done via ws events if available
+    interval(30000).subscribe(() => this._userRefresh$.next());
+  }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this._apiBaseUrl + "/users");
+    return this._userRefresh$.pipe(switchMap(() =>
+      this.http.get<User[]>(this._apiBaseUrl + "/users")));
+  }
+
+  createUser(userData: NewUser): Observable<User> {
+    // Post user data and refresh user listing
+    return this.http.post<User>("localhost", userData).pipe(tap(() => this._userRefresh$.next()));
   }
 }
