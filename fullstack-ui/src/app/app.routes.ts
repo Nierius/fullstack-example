cb68aea6cf8b196ba80b62c53910fe07f3df5cac
Marius Niemenmaa <marius.niemenmaa@outlook.com>
2025-03-15 10:26:52 +0200

    Simple page not found component

--- fullstack-ui/src/app/app.routes.ts
+++ fullstack-ui/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: UserListComponent,
-  }];
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  }
+];
---
+++ fullstack-ui/src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  templateUrl: './page-not-found.component.html',
+  imports: [MatButtonModule, RouterModule],
+  styleUrl: './page-not-found.component.scss'
+})
+export class PageNotFoundComponent { }
